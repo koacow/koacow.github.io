2e823f1f4dcf430c6e71d64f6eab8e35e28fa033
Khoa Cao <146030988+koacow@users.noreply.github.com>
2024-03-03 07:48:01 -0500

    Added logo and logo popup. Fixed some more scaling issues. Very close to being able to launch yay!

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const popupContainer = document.querySelector(".popup");
+popupContainer.addEventListener("click",() => {togglePopup()});
 
+function togglePopup() {
+    const popup = document.querySelector("#logoPopup");
+    popup.classList.toggle("show");
+}
