88fa86141202f386a4c634801bcba142429a402c
koacow <khoa.cnd@gmail.com>
2024-04-24 21:31:21 -0400

    added project description expanding feature

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const popupContainer = document.querySelector(".popup");
+const projectExpandables = document.querySelectorAll("h3.expand-toggle");
+
 popupContainer.addEventListener("click",() => {togglePopup()});
+projectExpandables.forEach(projectExpandToggle => {
+    projectExpandToggle.addEventListener("click",(event) => {expandProject(event)});
+});
 
 function togglePopup() {
     const popup = document.querySelector("#logoPopup");
     popup.classList.toggle("show");
 }
+
+expandProject = event => {
+    const project = event.target.nextElementSibling;
+    console.log(event.target)
+    project.classList.toggle("hidden");
+}
+
+
